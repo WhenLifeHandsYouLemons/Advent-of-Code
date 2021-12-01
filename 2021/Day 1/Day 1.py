f0aa06a6d2b8f06443034640d07b29dbf7695b15
Sooraj Sannabhadti <2005sooraj@gmail.com>
2021-12-01 18:55:47 +0800

    Day 1 Completed

--- 2021/Day 1/Day 1.py
+++ 2021/Day 1/Day 1.py
@@ -15,15 +15,17 @@ with open("C:/Users/2005s/Desktop/Advent-of-Code/2021/Day 1/Day 1 Resources.txt"
 increased = 0
 decreased = 0
 
-while line_no != len(all_lines)-1:
-    # print(all_lines[line_no])
+while line_no != len(all_lines)-3:
 
-    if all_lines[line_no] < all_lines[line_no+1]:
+    first_sum = all_lines[line_no] + all_lines[line_no+1] + all_lines[line_no+2]
+    second_sum = all_lines[line_no+1] + all_lines[line_no+2] + all_lines[line_no+3]
+
+    if first_sum < second_sum:
         increased = increased + 1
-        print(f"{all_lines[line_no+1]} has increased from {all_lines[line_no]}")
-    if all_lines[line_no] > all_lines[line_no+1]:
+        print(f"{second_sum} has increased from {first_sum}")
+    if first_sum > second_sum:
         decreased = decreased + 1
-        print(f"{all_lines[line_no+1]} has decreased from {all_lines[line_no]}")
+        print(f"{second_sum} has decreased from {first_sum}")
 
     line_no = line_no + 1
 
