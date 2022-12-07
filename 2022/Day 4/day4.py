8c885e97f73bfddde65be6a7d97e835b9ba73e29
Sooraj Sannabhadti <2005sooraj@gmail.com>
2022-12-07 08:23:50 +0800

    Day 4 & Day 5

--- 2022/Day 4/day4.py
+++ 2022/Day 4/day4.py
@@ -16,13 +16,19 @@ while line_no != len(all_lines):
     pairs = all_lines[line_no].split(",")
     first_pair = pairs[0].split("-")
     second_pair = pairs[1].split("-")
+    first_array = []
+    second_array = []
 
-    # Check if second pair is inside the first
-    if int(second_pair[0]) >= int(first_pair[0]) and int(second_pair[1]) <= int(first_pair[1]):
-        duplicates += 1
-    # Check if first pair is inside second pair
-    elif int(second_pair[0]) <= int(first_pair[0]) and int(second_pair[1]) >= int(first_pair[1]):
-        duplicates += 1
+    for i in range(int(first_pair[0]), int(first_pair[1])+1):
+        first_array.append(i)
+
+    for i in range(int(second_pair[0]), int(second_pair[1])+1):
+        second_array.append(i)
+
+    for i in first_array:
+        if i in second_array:
+            duplicates += 1
+            break
 
     line_no = line_no + 1
 
