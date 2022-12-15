859e1da985e0e5e28b9add9837a76ca917b176df
Sooraj Sannabhadti <2005sooraj@gmail.com>
2022-12-15 09:24:27 +0800

    Day 14 Part 1

--- 2022/Day 13/day13.py
+++ 2022/Day 13/day13.py
@@ -9,9 +9,10 @@ with open("2022/Day 13/data.txt", "r") as f:
     content = f.read()
     lines = content.splitlines()
     for line in lines:
-        all_lines.append(int(line))
-print(all_lines)
+        all_lines.append(line)
 
-while line_no != len(all_lines):
-    
-    line_no += 1
+while line_no <= len(all_lines)-2:
+    pair1 = eval(all_lines[line_no])
+    pair2 = eval(all_lines[line_no+1])
+    print(pair1, pair2)
+    line_no += 3
